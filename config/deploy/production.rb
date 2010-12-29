c773f9a8ecf49007671fe1ce2d463a4d64e6fe90
James Mead <james@floehopper.org>
2010-12-29 19:35:46 +0000

    I don't think this capistrano environment variable is used anywhere.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,5 @@
 server 'argonaut.slice', :app, :web, :db, :primary => true
 
-set :environment, 'production'
-
 manifest :app, %{
   floehopper::webby_app {'hannahsmithson.org':
     deploy_to => "<%= deploy_to %>",
